818637fcb9a7b8e4278388f0638cd29c9ee1fd56
Ilse Alejo <ilse.alejo@gmail.com>
2017-03-20 23:57:44 +0900

    Added show of articles with the hashtag i'es noticia'

--- app/controllers/titlepage_controller.rb
+++ app/controllers/titlepage_controller.rb
@@ -1,7 +1,7 @@
 class TitlepageController < ApplicationController
 	def index
 		search = Hashtag.find_by_name("#ESNOTICIA")
-		@its_news = ArticlesHashtag.where(hashtag_id: search)
+		@its_news = ArticlesHashtag.where(hashtag_id: search).last(10)
 		@sections = Section.all
 	end
 end
