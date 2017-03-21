d3b9a2fd054ca5942af037f8a92f896a1d67973a
Ilse Alejo <ilse.alejo@gmail.com>
2017-03-21 16:22:45 +0900

    Added al momento section

--- app/controllers/titlepage_controller.rb
+++ app/controllers/titlepage_controller.rb
@@ -6,24 +6,7 @@ class TitlepageController < ApplicationController
 		@its_news = ArticlesHashtag.where(hashtag_id: search).last(10)
 		#@sections = Section.articles.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articable_id").where('highlights.article_id IS NULL')
 
-		@san_luis = Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 1').last(5)
-
-		@seguridad = Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 2').last(5)
-
-		@nacional = Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 3').last(5)
-
-		@internacional= Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 4').last(5)
-
-		@farandula= Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 5').last(5)
-
-		@tactica = Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 6').last(5)
-
-		@negocios = Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 7').last(5)
-
-		@estados = Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 8').last(5)
-
-		@actualidad = Article.joins("LEFT OUTER JOIN highlights ON highlights.article_id = articles.id").where('highlights.article_id IS NULL AND articable_id == 8').last(5)
-
+		
 
 
 
