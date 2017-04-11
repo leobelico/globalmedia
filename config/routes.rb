7b5cae09b67cb9f21d67972a9b9e7c5b9c4a2a6d
elujambio <eugeniolujambio@gmail.com>
2017-04-11 15:00:50 -0500

    started general form stylings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  # Todas las rutas del panel tiene que tener panel
+  devise_scope :user do
+    get 'panel/login', to: 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "titlepage#index"
   resources :articles, param: :slug do
