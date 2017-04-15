08c2be61d7f2b8fc491d4575b1a0007309c61a5d
Pablo Lujambio <eugeniolujambio@gmail.com>
2017-04-14 22:45:36 -0500

    Quill Initializer modifications

--- app/assets/javascripts/components/quill-initializer.js
+++ app/assets/javascripts/components/quill-initializer.js
@@ -12,23 +12,6 @@ var toolbarOptions = [
   [{ 'size': ['small', false, 'large', 'huge'] }]  // custom dropdown
 
 ];
-// function resizeIFrameToFitContent( iFrame ) {
-
-//     iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
-//     iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
-// }
-
-// window.addEventListener('DOMContentReady', function(e) {
-//     var iFrame = document.getElementById( 'iFrame1' );
-//     resizeIFrameToFitContent( iFrame );
-
-//     // or, to resize all iframes:
-//     var iframes = document.querySelectorAll("iframe");
-//     for( var i = 0; i < iframes.length; i++) {
-//         resizeIFrameToFitContent( iframes[i] );
-//     }
-// } );
-
 
 var options = {
   theme: 'snow',
