3e23e1d932531f768ce24e250c5e3e084ff45136
elujambio <eugeniolujambio@gmail.com>
2017-04-15 15:52:45 -0500

    started styling tables

--- app/assets/javascripts/components/quill-forms.js
+++ app/assets/javascripts/components/quill-forms.js
@@ -1,38 +1,44 @@
 $(document).on("click", function(event){
   if ( $(event.target).is(".saver--article") ) {
-    show_button_spinner($(event.target));
-
-    console.log(editor.getContents()) 
-    console.log(editor.getText())
-
-
-
-
-    $.ajax({
-      url: '/path/to/file',
-      type: 'default GET (Other values: POST)',
-      dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-      data: {
-        name: $("#article_name").val(),
-        short_description: $("#article_short_description").val(),
-        articable_type: $("#article_articable_type").val(),
-        hashtag_names: $("#article_hashtags_names").val(), 
-        note: editor.getContents(), 
-        plain_text: editor.getText()
-      },
-    })
-    .done(function() {
-      console.log("success");
-    })
-    .fail(function() {
-      console.log("error");
-    })
-    .always(function() {
-      console.log("complete");
-    });
+    submit_form(); 
     
   }
 }); 
+
+function submit_form(){
+   show_button_spinner($(event.target));
+
+  console.log(editor.getContents()) 
+  console.log(editor.getText())
+
+
+  var quill_note = editor.getContents();
+  var text_note = editor.getText();
+  $.ajax({
+    url: '/panel/articles',
+    type: 'POST',
+    data: {
+      article: {
+        name: $("#article_name").val(),
+        short_description: $("#article_short_description").val(),
+        articable_id: $("#article_articable_id").val(),
+        articable_type: $("#article_articable_type").val(),
+        hashtag_names: $("#article_hashtags_names").val(), 
+        note: quill_note.opts, 
+        plain_text: text_note          
+      }
+    }
+  })
+  .done(function() {
+    console.log("success");
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+}
 
 function show_button_spinner(button) {
   TweenMax.to(button.find(".button--text"), .3, {
