24e796a5946cc8ff9a1f0b46980d333b51ade90f
elujambio <eugeniolujambio@gmail.com>
2017-05-03 12:58:09 -0500

    added animation for menu button

--- app/assets/javascripts/components/mobile-menu.js
+++ app/assets/javascripts/components/mobile-menu.js
@@ -1,6 +1,16 @@
-menu_open = false
-$(document).on("click", function {
-	if ($(event.target).is(".menu-trigger") && menu_open == false) {
-		// TweenMax.to("");
+menu_open = false;
+$(document).on("click", function(event){
+	if ( $(event.target).is(".menu-trigger") && menu_open == false ) {
+		menu_open = true; 
+		TweenMax.to(".mt-1", .2, { top: "+=9", rotation: 45 });
+		TweenMax.to(".mt-2", .2, { opacity: 0 });
+		TweenMax.to(".mt-3", .2, { top: "-=9", rotation: -45 });
+	} else {
+		if ( $(event.target).is(".menu-trigger") && menu_open == true ) {
+			menu_open = false; 
+			TweenMax.to(".mt-1", .2, { top: "-=9", rotation: 0 });
+			TweenMax.to(".mt-2", .2, { opacity: 1 });
+			TweenMax.to(".mt-3", .2, { top: "+=9", rotation: 0 });	
+		}
 	}
 });
