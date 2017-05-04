c3539b3c0b066bf08cf44ccb31344127051aaac4
elujambio <eugeniolujambio@gmail.com>
2017-05-04 13:54:19 -0500

    changed metatags for whatsapp debug

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,4 +1,12 @@
 class Timetable < ApplicationRecord
 	validates :name, :image, :streaming_hour, :end_streaming_hour, :broadcasters, presence: true
 	belongs_to :station, optional: true
+	validate :review_amazon_on_timetable_images
+
+	private 
+		def review_amazon_on_timetable_images	
+			if !self.image.include? "amazon" or !self.broadcaster_image.include? "amazon"
+				errors.add(:base, "La imagen no se subió bien, vuelve a intentar.")
+			end
+		end
 end
