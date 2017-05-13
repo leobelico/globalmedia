708720601c4f7009d9b1fe073af11e9f4744ab4b
elujambio <eugeniolujambio@gmail.com>
2017-05-13 18:12:04 -0500

    corrected query for timetables in show

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -7,40 +7,37 @@ class StationsController < ApplicationController
 	end
 	def show
 		@recommendations = Station.where("id != '#{@station.id}'").last(7) 
-	    time = Time.now 
+	    time = Time.now
 	    if time.sunday? 
-	      @timetables = Timetable.where("sunday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
+	      @timetables = Timetable.where("(sunday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') OR streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
 	    end
 	   
 	    if time.monday? 
-	      @timetables = Timetable.where("monday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
+	      @timetables = Timetable.where("(monday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') OR streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
 	    end
 	   
 	    if time.tuesday? 
-	      @timetables = Timetable.where("tuesday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
+	      @timetables = Timetable.where("(tuesday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') OR streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
 	    end
 	   
 	    if time.wednesday? 
-	      @timetables = Timetable.where("wednesday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
+	      @timetables = Timetable.where("(wednesday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') OR streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
 	    end
 	   
 	    if time.thursday? 
-	      @timetables = Timetable.where("thursday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
+	      @timetables = Timetable.where("(thursday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') OR streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
 	    end
 	   
 	    if time.friday? 
-	      @timetables = Timetable.where("friday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
+	      @timetables = Timetable.where("(friday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') OR streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
 	    end
 	   
 	    if time.saturday? 
-	      @timetables = Timetable.where("saturday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
+	      @timetables = Timetable.where("(saturday = 'true' AND (streaming_hour < '#{time}' AND end_streaming_hour > '#{time}') OR streaming_hour > '#{time}') AND station_id = '#{@station.id}'").first(5)
 	    end
 	   
-	    @timetables.each do |t|
-	    	p t.name 
-	    	p t.streaming_hour
-	    	p t.end_streaming_hour
-	    end
+	    p @timetables
+
 	end
 
 	
