93bd95e917a1cc80f0f5f1e9f526af274bdde743
elujambio <eugeniolujambio@gmail.com>
2017-05-24 17:03:42 -0500

    added title to hits graph

--- app/controllers/panel/hits_controller.rb
+++ app/controllers/panel/hits_controller.rb
@@ -3,7 +3,7 @@ class Panel::HitsController < ApplicationController
 	before_action :check_hits_permission
 	# load_and_authorize_resource
 	def index
-		@hits = Hit.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month)
+		@hits = Hit.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).paginate(page: params[:page], per_page: 20)
 		@hit_objectives = HitObjective.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month)	
 	end
 	def graph
