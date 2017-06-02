7a605915b82bb02a2a96074d26e4967b0bb48d46
elujambio <eugeniolujambio@gmail.com>
2017-06-02 12:16:07 -0500

    deleted migration

--- db/migrate/20170529041203_add_note_old_to_articles.rb
+++
@@ -1,8 +0,0 @@
-# class AddNoteOldToArticles < ActiveRecord::Migration[5.0]
-#   def change
-#     add_column :articles, :note_old, :text, default: ""
-#     add_column :articles, :old_id, :integer
-#     add_column :sections, :old_id, :integer
-#     add_column :images, :old_id, :integer
-#   end
-# end
