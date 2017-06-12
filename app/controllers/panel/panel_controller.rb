a7449ad13f17667d6163f864ec172da82abebf51
elujambio <eugeniolujambio@gmail.com>
2017-06-12 13:33:14 -0500

    corrected global recommendations error

--- app/controllers/panel/panel_controller.rb
+++ app/controllers/panel/panel_controller.rb
@@ -10,15 +10,15 @@ class Panel::PanelController < ApplicationController
 	def set_global_recommendations
 		articles_in_highlights = Article.where(global_recommendation: true)
 		articles_in_highlights.update_all(global_recommendation: false)
-		if params[:panel][:first_article_id]
+		if params[:panel][:first_article_id] != ""
 			first_article = Article.find(params[:panel][:first_article_id])
 			first_article.update_attributes(global_recommendation: true)
 		end
-		if params[:panel][:second_article_id]
+		if params[:panel][:second_article_id]  != ""
 			second_article = Article.find(params[:panel][:second_article_id])
 			second_article.update_attributes(global_recommendation: true)
 		end
-		if params[:panel][:third_article_id]
+		if params[:panel][:third_article_id]  != ""
 			third_article = Article.find(params[:panel][:third_article_id])
 			third_article.update_attributes(global_recommendation: true)
 			
