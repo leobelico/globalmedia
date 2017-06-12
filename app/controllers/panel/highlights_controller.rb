ad417c7c718c727ab556ef57f258e617a210a008
elujambio <eugeniolujambio@gmail.com>
2017-06-12 13:54:42 -0500

    changed highlights order

--- app/controllers/panel/highlights_controller.rb
+++ app/controllers/panel/highlights_controller.rb
@@ -7,7 +7,7 @@ class Panel::HighlightsController < ApplicationController
 	autocomplete :article, :name, full: true
 	before_action :get_all_highlights, only: [:new, :create, :edit, :update]
 	def index
-		@highlights = Highlight.where("highlights.order < 7").order(order: "ASC", published: "DESC")
+		@highlights = Highlight.where("highlights.order < 7").order(published: "DESC", order: "ASC")
 	end
 
 	def new
