35ea3601f9a0f3e226167ecbee4ad571aa240f7d
elujambio <eugeniolujambio@gmail.com>
2017-06-17 14:36:03 -0500

    added assets for production, radio banner tune in text made smaller, added menu text to menu trigger

--- app/assets/javascripts/components/mobile-menu.js
+++ app/assets/javascripts/components/mobile-menu.js
@@ -1,13 +1,13 @@
 menu_open = false;
 $(document).on("touchend", function(event){
-	if ( $(event.target).is(".menu-trigger") && menu_open == false ) {
+	if ( $(event.target).is(".menu-button") && menu_open == false ) {
 		menu_open = true; 
 		TweenMax.to(".mt-1", .2, { y: "+=9", rotation: 45 });
 		TweenMax.to(".mt-2", .2, { opacity: 0 });
 		TweenMax.to(".mt-3", .2, { y: "-=9", rotation: -45 });
 		TweenMax.to(".mobile-menu", .4, { y: "100%" });
 	} else {
-		if ( $(event.target).is(".menu-trigger") && menu_open == true ) {
+		if ( $(event.target).is(".menu-button") && menu_open == true ) {
 			menu_open = false; 
 			TweenMax.to(".mt-1", .2, { y: "-=9", rotation: 0 });
 			TweenMax.to(".mt-2", .2, { opacity: 1 });
