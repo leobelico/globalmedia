0d7e8839ecceee20f018a5c263c6917eb30bc37c
elujambio <eugeniolujambio@gmail.com>
2017-08-29 14:08:17 -0500

    corrected banners query

--- app/controllers/api/v1/banners_controller.rb
+++ app/controllers/api/v1/banners_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::BannersController < Api::BaseController
 	def get_banners 
 	    time = DateTime.now.to_date
 	    if params[:type] and params[:id] 
-			@banners = Banner.joins("INNER JOIN section_banners ON section_banners.banner_id = banners.id AND section_banners.sectionable_type = #{params[:type]} AND expiry_date > '#{time}' AND section_banners.sectionable_id = #{params[:id]}")
+			@banners = Banner.joins("INNER JOIN section_banners ON section_banners.banner_id = banners.id AND section_banners.sectionable_type = '#{params[:type]}' AND expiry_date > '#{time}' AND section_banners.sectionable_id = #{params[:id]}")
 		end
 		if @banners 
 			json_response(@banners, :ok)
