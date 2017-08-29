33d01871145fe4da5debdc2f717553dcabfe803d
elujambio <eugeniolujambio@gmail.com>
2017-08-29 14:27:39 -0500

    added description to stations serializer

--- app/serializers/station_serializer.rb
+++ app/serializers/station_serializer.rb
@@ -1,4 +1,4 @@
 class StationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :stream_url, :image, :frequency, :image_preview, :twitter, :facebook, :video, :news, :playlist_1, :playlist_1_image, :playlist_2, :playlist_2_image, :playlist_3, :playlist_3_image, :playlist_4, :playlist_4_image
+  attributes :id, :name, :description, :stream_url, :image, :frequency, :image_preview, :twitter, :facebook, :video, :news, :playlist_1, :playlist_1_image, :playlist_2, :playlist_2_image, :playlist_3, :playlist_3_image, :playlist_4, :playlist_4_image
   has_many :timetables, serializer: TimetableSerializer
 end
