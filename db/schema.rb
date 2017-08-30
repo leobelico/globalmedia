acb4cea76a9afab6218e0698f47d6868fc340692
Ilse Mireya Alejo Gutiérrez <ilse.alejo@gmail.com>
2017-08-29 21:14:25 -0500

    resgister notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823141826) do
+ActiveRecord::Schema.define(version: 20170830014239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,6 +173,13 @@ ActiveRecord::Schema.define(version: 20170823141826) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "slug",       default: ""
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "podcasts", force: :cascade do |t|
