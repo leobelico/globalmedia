fb0cfd00b14aa865669a7b50b46aec568e27bb03
Ilse Mireya Alejo Gutiérrez <ilse.alejo@gmail.com>
2017-08-29 21:36:07 -0500

    modify token creation

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DevicesController < Api::BaseController
 	def create 
-		@device = Device.create!(device_params)
+		@device = Device.create!(token: params[:token], operating_system: params[:operating_system])
 		json_response(@device, :created)
 	end
 	private 
