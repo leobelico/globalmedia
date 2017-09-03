33e08fc95d415c43f552d7e4b7ace61e45c2a70f
Ilse Mireya Alejo Gutiérrez <ilse.alejo@gmail.com>
2017-09-03 00:54:18 -0500

    Added notis for android andandroid and ios

--- app/controllers/panel/notifications_controller.rb
+++ app/controllers/panel/notifications_controller.rb
@@ -16,53 +16,55 @@ class Panel::NotificationsController < ApplicationController
 	
 		@notification = Notification.new(notification_params)
 		if @notification.save
-			sns = Aws::SNS::Client.new
+			# sns = Aws::SNS::Client.new
 
-	Device.where(operating_system: "ios").each do |device|
+	# Device.where(operating_system: "ios").each do |device|
 		
-		p "Device token"
-		p device.token
-		endpoint = sns.create_platform_endpoint(platform_application_arn: "arn:aws:sns:us-west-1:123129123171:app/APNS/GlobalMediaProd", token: device.token)
+	# 	p "Device token"
+	# 	p device.token
+	# 	endpoint = sns.create_platform_endpoint(platform_application_arn: "arn:aws:sns:us-west-1:123129123171:app/APNS/GlobalMediaProd", token: device.token)
 		
 		
-		if @notification.wants_keys?
-			p "quiere llaves"
-			iphone_notification = {
-			aps: {
-				alert: @notification.body, sound: true, bagde: 0, extra: { key: @notification.key,
-            	key_id: @notification.key_id }
-				}
-			}
-		else
-			iphone_notification = {
-				aps: {
-					alert: @notification.body, sound: true, bagde: 0, extra: {}
-				}
-			}
-		end
+	# 	if @notification.wants_keys?
+	# 		p "quiere llaves"
+	# 		iphone_notification = {
+	# 		aps: {
+	# 			alert: @notification.body, sound: true, bagde: 0, extra: { key: @notification.key,
+ #            	key_id: @notification.key_id }
+	# 			}
+	# 		}
+	# 	else
+	# 		iphone_notification = {
+	# 			aps: {
+	# 				alert: @notification.body, sound: true, bagde: 0, extra: {}
+	# 			}
+	# 		}
+	# 	end
 		
 
-		message = {
-			default: @notification.body,
-			APNS_SANDBOX: iphone_notification.to_json,
-			APNS: iphone_notification.to_json
-		}
-		p "endpoint 2" 
-		p endpoint
-		begin
-			resp = sns.publish({
-				target_arn: endpoint.endpoint_arn,
-				message: message.to_json,
-				message_structure: "json"
-				}
+	# 	message = {
+	# 		default: @notification.body,
+	# 		APNS_SANDBOX: iphone_notification.to_json,
+	# 		APNS: iphone_notification.to_json
+	# 	}
+	# 	p "endpoint 2" 
+	# 	p endpoint
+	# 	begin
+	# 		resp = sns.publish({
+	# 			target_arn: endpoint.endpoint_arn,
+	# 			message: message.to_json,
+	# 			message_structure: "json"
+	# 			}
 						
-			)
-		rescue => ex
-			p "error notis"
-			logger.error ex.message
-		end
-	end
-			# SendNotificationsJob.perform_later @notification
+	# 		)
+	# 	rescue => ex
+	# 		p "error notis"
+	# 		logger.error ex.message
+	# 	end
+	# end
+
+		
+			SendNotificationsJob.perform_later @notification
 			redirect_to panel_notifications_path
 		else
 			render action: "new"
--- app/jobs/send_notifications_job.rb
+++ app/jobs/send_notifications_job.rb
@@ -6,12 +6,11 @@ class SendNotificationsJob < ApplicationJob
 
 	Device.where(operating_system: "ios").each do |device|
 		
-			
 		endpoint = sns.create_platform_endpoint(platform_application_arn: "arn:aws:sns:us-west-1:123129123171:app/APNS/GlobalMediaProd", token: device.token)
 		
 		
 		if notification.wants_keys?
-			p "quiere llaves"
+			
 			iphone_notification = {
 			aps: {
 				alert: notification.body, sound: true, bagde: 0, extra: { key: notification.key,
@@ -32,14 +31,56 @@ class SendNotificationsJob < ApplicationJob
 			APNS_SANDBOX: iphone_notification.to_json,
 			APNS: iphone_notification.to_json
 		}
+	
+		begin
+			resp = sns.publish({
+				target_arn: endpoint.endpoint_arn,
+				message: message.to_json,
+				message_structure: "json"
+				}
+						
+			)
+		rescue => ex
+			p "error notis"
+			logger.error ex.message
+		end
+	end
+
+	Device.where(operating_system: "android").each do |device|
+	
+		endpoint = sns.create_platform_endpoint(platform_application_arn: "arn:aws:sns:us-west-1:123129123171:app/GCM/android2", token: device.token)
 		
-		resp = sns.publish({
-			target_arn: endpoint.endpoint_arn,
-			message: message.to_json,
-			message_structure: "json"
+		
+		if notification.wants_keys?
+			p "quiere llaves"
+			message = {
+				default: { data: notification.body }.to_json,
+				GCM: { data: { message: notification.body, key: notification.key, key_id: notification.key_id} }.to_json
 			}
-					
-		)
+		else
+			message = {
+				default: { data: notification.body }.to_json,
+				GCM: { data: { message: notification.body } }.to_json
+			}
+		end
+		
+		#{
+# "GCM": "{ \"data\": { \"message\": \"hola\", \"key\": \"article\", \"key_id\": \"440\" } }"
+# }
+		
+	
+		begin 
+			resp = sns.publish({
+				target_arn: endpoint.endpoint_arn,
+				message: message.to_json,
+				message_structure: "json"
+				}
+						
+			)
+		rescue => ex
+			p "error notis"
+			logger.error ex.message
+		end
 	end
   end
 end
