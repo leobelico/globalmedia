7c20693986c69e2d7b395bf029bb22f67997bbc3
elujambio <eugeniolujambio@gmail.com>
2017-09-11 11:56:10 -0500

    changed word break to no break in highlights

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -55,7 +55,7 @@ class SectionsController < ApplicationController
    			most_visited << article.id
    		end
    		@highlight = Article.where(articable_id: @section.id, highlight: true, published: true).order(updated_at: "DESC").first
-  		@articles = Article.find(articles - r_articles - most_visited).paginate(page: params[:page], per_page: 21)
+  		@articles = Article.find(articles - r_articles).paginate(page: params[:page], per_page: 21)
 
 	end
 
