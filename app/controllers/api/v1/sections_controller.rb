39975975aa9ab09bdd70586b120a68117a42100c
Ilse Mireya Alejo Gutiérrez <ilse.alejo@gmail.com>
2017-09-12 13:25:19 -0500

    changed order api

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SectionsController < Api::BaseController
 	    json_response(@sections, :ok)
 	end
 	def show 
-    	paginate json: @section.articles.where(published: true), adapter: :json, per_page: 20		
+    	paginate json: @section.articles.where(published: true).order(created_at: "DESC"), adapter: :json, per_page: 20		
 	end
 
 	def get_articles 
