5874daae558a8efbf50cf66d8c08d4ce9245dd97
Ilse Mireya Alejo Gutiérrez <ilse.alejo@gmail.com>
2017-09-20 13:26:27 -0500

    added redis

--- config/initializers/profiler.rb
+++ config/initializers/profiler.rb
@@ -3,3 +3,5 @@
 #   Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port, :password => uri.password }
 #   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 # end
+
+Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
