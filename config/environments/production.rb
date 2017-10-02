05d6cdd656953612f5e8d4e8b19ac447cd334aa7
Ilse Mireya Alejo Gutiérrez <ilse.alejo@gmail.com>
2017-10-02 16:03:57 -0500

    Added dalli gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.cache_store = :dalli_store
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
