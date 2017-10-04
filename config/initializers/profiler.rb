6cd99ddeb4fae8dcd6011e94ab13da37d9f14296
Ilse Mireya Alejo Gutiérrez <ilse.alejo@gmail.com>
2017-10-04 15:33:04 -0500

    solved

--- config/initializers/profiler.rb
+++ config/initializers/profiler.rb
@@ -1,4 +1,4 @@
-Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+# Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
 
 if Rails.env.production?
   # uri = URI.parse("//redistogo:1293abfb0e0c941b3a8badf16c88182c@angelfish.redistogo.com:10926/")
