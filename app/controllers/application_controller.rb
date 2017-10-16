1bdbb40d0dc23017df47ad810239d8a7a45ede43
Ilse Alejo <ilse.alejo@gmail.com>
2017-10-15 20:12:49 -0500

    Added limit 100

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -309,7 +309,7 @@ class ApplicationController < ActionController::Base
   def most_visited
     #@hits = Hit.where(created_at: 2.hours.ago..Time.now).order(number: "ASC").last(3)
 
-    @articles = Article.limit(4000).joins("LEFT OUTER JOIN hits ON hits.article_id = articles.id").where("articles.published = true AND articles.highlight = false AND articles.global_recommendation = ? AND hits.created_at > ? AND hits.created_at < ?", false,2.hours.ago, Time.now).order("hits.number").last(3)
+    @articles = Article.joins("LEFT OUTER JOIN hits ON hits.article_id = articles.id").where("articles.published = true AND articles.highlight = false AND articles.global_recommendation = ? AND hits.created_at > ? AND hits.created_at < ?", false,2.hours.ago, Time.now).order("hits.number").last(3)
 
     # Táctica Nacional, Internacional, Farándula, Entretenimiento 
 
