de292c6db1c40061bd14d12d6dfea526d6dce497
elujambio <eugeniolujambio@gmail.com>
2017-12-13 14:00:24 -0600

    fixed url for production password management

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-   config.action_mailer.default_url_options = { :host => 'global-media.herokuapp.com' }
+   config.action_mailer.default_url_options = { :host => 'https://www.globalmedia.mx' }
  
 
   config.action_mailer.delivery_method = :smtp
