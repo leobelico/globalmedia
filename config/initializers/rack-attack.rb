8b2af28976745003d3a9720a07bde954f06a7ec4
Ilse Alejo <ilse.alejo@gmail.com>
2017-12-16 22:55:40 -0600

    corrected blocked ip

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,4 +1,17 @@
-Rack::Attack.blocklist('block 52.233.27.230') do |req|
-  # Requests are blocked if the return value is truthy
-  '52.233.27.230' == req.ip
+class Rack::Attack
+	Rack::Attack.blocklist('block 52.233.27.230') do |req|
+	  # Requests are blocked if the return value is truthy
+	  '52.233.27.230' == req.ip
+	end
+
+	Rack::Attack.throttle('req/ip', :limit => 5, :period => 1.second) do |req|
+	  # If the return value is truthy, the cache key for the return value
+	  # is incremented and compared with the limit. In this case:
+	  #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
+	  #
+	  # If falsy, the cache key is neither incremented nor checked.
+
+	  req.ip
+	end
+
 end
