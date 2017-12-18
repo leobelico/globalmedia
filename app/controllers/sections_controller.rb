6b958c7885955dbf5cb6ebd7ffb43b4de552ef22
Ilse Alejo <ilse.alejo@gmail.com>
2017-12-17 23:08:38 -0600

    improved section show

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -42,7 +42,7 @@ class SectionsController < ApplicationController
 		@related_sections = RelatedSection.where(section: @section)	
        
     	@first_article = Article.joins("INNER JOIN cover_articles ON cover_articles.article_id = articles.id").where("cover_articles.section_id = #{@section.id}").order("cover_articles.article_highlight desc, cover_articles.published_at desc").first
-  		@articles = Article.where("published = true AND articable_id = ? AND published_at IS NOT NULL AND id != ?", @section.id, @first_article.id).order(published_at: :desc).paginate(page: params[:page], per_page: 13)
+  		@articles = Article.where("published = true AND articable_id = ? AND published_at IS NOT NULL AND id != ?", @section.id, @first_article.id).order(published_at: :desc).paginate(page: params[:page], per_page: 12)
 
 	end
 
