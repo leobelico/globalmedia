2eb0a3c1ba60de0aee1a186bba0700c134fb2a6c
nosoyilse <ilse.alejo@gmail.com>
2018-07-09 18:05:38 +0900

    Added sports functionality

--- app/controllers/api/v1/sports_controller.rb
+++ app/controllers/api/v1/sports_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SportsController < Api::BaseController
 	def index
 		@sports = Sport.all.order(created_at: "DESC") 
+		p "SPORTSSS"
+		p @sports
     	paginate json: @sports, adapter: :json, per_page: 20
 	end
 	
