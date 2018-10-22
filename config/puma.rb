b303687aa3f6dcf003bcea248f3ea9177e2c085a
simongm3 <simon.banda@gmnet.mx>
2018-10-22 17:08:13 -0500

    readded concurrency to puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 
 # Commenting due to windows incompatibility, should be enabled in production
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
