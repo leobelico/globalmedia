ea522325ffa64f8de1fc37b6d5dbd0d17782f46d
Pablo Eugenio Lujambio <eugeniolujambio@gmail.com>
2020-02-17 15:48:14 -0600

    corrected slug link in article sindex

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,16 +1,16 @@
 class Section < ApplicationRecord
 	has_many :articles, as: :articable
-  	before_create :to_slug
-  	has_many :section_highlights
-  	has_many :section_banners, as: :seleccionable
-  	has_many :hits_objectives
+	before_create :to_slug
+	has_many :section_highlights
+	has_many :section_banners, as: :seleccionable
+	has_many :hits_objectives
 	validates :name, presence: true
 	# has_many :related_sections
 	has_many :related_sections, class_name: 'RelatedSection', :foreign_key => 'section_reference'
 
 	def to_param
-    	slug
-  	end
+  	slug
+	end
 
   	def total_hits_in_current_month
     	Hit.joins("INNER JOIN articles ON articles.id = hits.article_id WHERE articles.articable_id = #{self.id} AND hits.created_at >= '#{Time.now.beginning_of_month}' AND hits.created_at <= '#{Time.now.end_of_month}'").sum(:number)
