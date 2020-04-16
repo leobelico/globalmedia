efeeb3bc711d9953baa9385314b2a0bc8be398cd
Jonatan <jndelarosar@gmail.com>
2020-04-16 15:22:55 -0500

    cambio de banner 05/04/2020

--- app/controllers/api/v1/podcasts_controller.rb
+++ app/controllers/api/v1/podcasts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PodcastsController < Api::BaseController
 	def index
-		@podcasts = Podcast.all.order(created_at: "DESC") 
+		@podcasts = Podcast.all.order(created_at: "DESC")
     	paginate json: @podcasts, adapter: :json, per_page: 20
 	end
 end
