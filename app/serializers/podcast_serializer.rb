68d1e12259d47866153f388315712cdabe170e9f
Jonatan <jndelarosar@gmail.com>
2020-05-21 17:57:53 -0500

    Descripcion podcast activa

--- app/serializers/podcast_serializer.rb
+++ app/serializers/podcast_serializer.rb
@@ -1,4 +1,4 @@
 class PodcastSerializer < ActiveModel::Serializer
   attributes :id, :name, :stream_url
-  belongs_to :station, each_serializer: StationSerializer 
+  belongs_to :station, each_serializer: StationSerializer
 end
