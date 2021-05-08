38099a6e9b96c3f1565d959f27d8d41b0d9b82d9
Eduardo <eduardo.quiroz@gmnet.mx>
2021-05-07 19:26:43 -0500

    Prueba de banner completo

--- app/controllers/titlepage_controller.rb
+++ app/controllers/titlepage_controller.rb
@@ -70,7 +70,7 @@ class TitlepageController < ApplicationController
 			if poll_id != nil
 				if location.key == 'san-luis'
 					@banner_poll['show_results'] = false
-					@banner_poll['poll_url'] = "https://sondeosglobalmedia.com.mx/globalmedia/?p=#{poll_id.value}"
+					@banner_poll['poll_url'] = ""
 				else
 					@banner_poll['poll_url'] = "https://sondeosglobalmedia.com.mx/globalmedia_#{location.key}/?p=#{poll_id.value}"
 				end
