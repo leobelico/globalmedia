54b1ea67a9e37824b1c445feb0b44047a6e9c5ab
Eduardo <eduardo.quiroz@gmnet.mx>
2021-07-05 07:52:31 -0500

    banner_whatsapp

--- app/controllers/titlepage_controller.rb
+++ app/controllers/titlepage_controller.rb
@@ -70,7 +70,7 @@ class TitlepageController < ApplicationController
 			if poll_id != nil
 				if location.key == 'san-luis'
 					@banner_poll['show_results'] = false
-					@banner_poll['poll_url'] = "https://www.globalmedia.mx/articles/Transmisi%C3%B3n-en-vivo-elecciones-2021"
+					@banner_poll['poll_url'] = "https://api.whatsapp.com/message/WIUIWFCDVWFMM1"
 				else
 					@banner_poll['poll_url'] = "https://sondeosglobalmedia.com.mx/globalmedia_#{location.key}/?p=#{poll_id.value}"
 				end
