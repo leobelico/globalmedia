05a7f8707e18dbd9180757b88bec2303353b4398
Eduardo <eduardo.quiroz@gmnet.mx>
2021-08-03 11:58:24 -0500

    Se sube banner de imagen informativa

--- app/controllers/titlepage_controller.rb
+++ app/controllers/titlepage_controller.rb
@@ -70,7 +70,7 @@ class TitlepageController < ApplicationController
 			if poll_id != nil
 				if location.key == 'san-luis'
 					#APARTADO PARA ACTIVAR EL MINI-BANNER - AQUI SE AGREGA LINK DEL BANNER GRANDE
-					@banner_poll['show_results'] = false
+					@banner_poll['show_results'] = true
 					@banner_poll['poll_url'] = "https://api.whatsapp.com/message/WIUIWFCDVWFMM1"
 				else
 					@banner_poll['poll_url'] = "https://sondeosglobalmedia.com.mx/globalmedia_#{location.key}/?p=#{poll_id.value}"
