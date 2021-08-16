43ec2f6f1d1a845c688b093f2e1bfd80bab83966
Eduardo <eduardo.quiroz@gmnet.mx>
2021-08-16 09:54:44 -0500

    ASI LAS COSAS

--- app/controllers/titlepage_controller.rb
+++ app/controllers/titlepage_controller.rb
@@ -71,7 +71,7 @@ class TitlepageController < ApplicationController
 				if location.key == 'san-luis'
 					#APARTADO PARA ACTIVAR EL MINI-BANNER - AQUI SE AGREGA LINK DEL BANNER GRANDE
 					@banner_poll['show_results'] = false
-					@banner_poll['poll_url'] = "https://www.globalmedia.mx/articles/DIEZ-A-LAS-DIEZ-EN-VIVO"
+					@banner_poll['poll_url'] = "https://www.globalmedia.mx/articles/AS%C3%8D-LAS-COSAS"
 				else
 					@banner_poll['poll_url'] = "https://sondeosglobalmedia.com.mx/globalmedia_#{location.key}/?p=#{poll_id.value}"
 				end
