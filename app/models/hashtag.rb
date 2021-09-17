5aee5b1f4c7357bc0cf693c7593a48f551de66bf
Eduardo <eduardo.quiroz@gmnet.mx>
2021-09-17 13:31:39 -0500

    The file hashtag.rb is modified and override the function find_or_create_by which creates a slug, which is created by the name of the hashtag Also the migration is added which creates the slug column in the hashtags table

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,20 @@
 class Hashtag < ApplicationRecord
 	has_and_belongs_to_many :articles
 
+	def self.find_or_create_by(name)
+		slug = I18n.transliterate(name)
+		slug.strip!
+		slug = slug.length < 1 ?  "default" : slug
+		slug = slug.scan(/([[:alnum:]]+)/).join('_')
+		slug = slug.length < 1 ?  "default" : slug
+		slug = slug.downcase
+		result = self.find_by(name: name)
+		if result == nil
+			result = self.create(name: name)
+			result.slug = slug == 'default' ? "#{result.id}" : slug
+			result.save
+		end
+		puts result.slug
+		result
+	end
 end
