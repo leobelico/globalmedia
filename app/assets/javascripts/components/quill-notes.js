176f12dbb2d426f15a0b90a5a89fdb18f929dc2c
Sistemas <ldj821@gmail.com>
2025-07-22 10:37:59 -0600

    mejoras en notas

--- app/assets/javascripts/components/quill-notes.js
+++ app/assets/javascripts/components/quill-notes.js
@@ -1,16 +1,19 @@
 function display_quill_note(quill_note, container) {
-	var options = {
-	  readOnly: true
-	}
-	// console.log(quill_note)
-	Parchment = Quill.import('parchment');
-	Delta = Quill.import('delta');
-	Quill.register(TweetBlot);
-	Quill.register(EmbedContentBlot);
-	Quill.register(InstagramBlot);
-	// Quill.register(InstagramBlot);
+  var options = {
+    readOnly: true,
+    theme: 'snow'
+  }
+  
+  // Importaciones necesarias
+  Parchment = Quill.import('parchment');
+  Delta = Quill.import('delta');
+  
+  // Registrar blots personalizados
+  Quill.register(TweetBlot);
+  Quill.register(EmbedContentBlot);
+  Quill.register(InstagramBlot);
 
-	editor = new Quill(container, options);
-
-	editor.setContents(quill_note);
+  // Usar window.editor para mantener consistencia
+  window.quillViewer = new Quill(container, options);
+  window.quillViewer.setContents(quill_note);
 }
