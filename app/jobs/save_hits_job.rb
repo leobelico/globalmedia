b1c0f07fec10151363c4f720bc30ffb560c31e7f
Sistemas <ldj821@gmail.com>
2025-07-30 08:26:17 -0600

    delete weather

--- app/jobs/save_hits_job.rb
+++ app/jobs/save_hits_job.rb
@@ -2,18 +2,13 @@ class SaveHitsJob < ApplicationJob
   queue_as :default
 
   def perform(article)
-    # Do something later
+    last_hit = Hit.where(article: article, created_at: 2.hours.ago..Time.now).last
 
-	last_hit = Hit.where(article: article, created_at: 2.hours.ago..Time.now).last
-
-	if last_hit
-		last_hit.update_attributes(number: last_hit.number + 1)
-
-	else
-				#no hay ningún hit entonces lo creamos
-		Hit.create(number: 1, article: article)
-
-
-	end
+    if last_hit
+      last_hit.update(number: last_hit.number + 1)
+    else
+      # No hay ningún hit, entonces lo creamos
+      Hit.create(number: 1, article: article)
+    end
   end
 end
