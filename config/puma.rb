2cbbe2965742c929cfeb14ccf75b7f8153995270
Sistemas <ldj821@gmail.com>
2025-07-31 17:16:36 -0600

    reaload quill

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # Puma puede atender múltiples solicitudes por hilo desde un pool interno.
 # Estas líneas definen el número mínimo y máximo de hilos usados por cada worker.
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 8 }.to_i
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 16 }.to_i
 threads threads_count, threads_count
 
 # Puerto en el que Puma escucha las peticiones (por defecto 3000)
@@ -11,7 +11,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Número de workers (procesos). Aumenta la concurrencia en máquinas con varios núcleos.
 # Render soporta esto. Si estás en local en Windows, esto puede causar errores (usa solo 1).
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Precarga la app antes de hacer fork de los workers (mejora uso de memoria con Copy-On-Write)
 preload_app!
