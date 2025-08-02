a8cad3a533cc081371c4b241f9f5fd5e0cc372e7
Sistemas <ldj821@gmail.com>
2025-08-01 19:01:24 -0600

    no ddos

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,52 +1,41 @@
-# class Rack::Attack
-# 	Rack::Attack.blocklist('block 52.233.27.230') do |req|
-# 	  # Requests are blocked if the return value is truthy
-# 	  ['52.233.27.230', '85.241.15.110', '90.94.138.253']== req.ip
-# 	end
+class Rack::Attack
+  # BLOQUEO DE IPS ESPECÍFICAS
+  Rack::Attack.blocklist('block known bad IPs') do |req|
+    [
+      '52.233.27.230',
+      '85.241.15.110',
+      '90.94.138.253'
+    ].include?(req.ip)
+  end
 
-# 	Rack::Attack.throttle('req/ip', :limit => 500, :period => 5.minutes) do |req|
-# 	  # If the return value is truthy, the cache key for the return value
-# 	  # is incremented and compared with the limit. In this case:
-# 	  #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
-# 	  #
-# 	  # If falsy, the cache key is neither incremented nor checked.
+  # LIMITAR PETICIONES POR IP (THROTTLING)
+  Rack::Attack.throttle('req/ip', limit: 500, period: 5.minutes) do |req|
+    req.ip
+  end
 
-# 	  req.ip
-# 	end
+  # BLOQUEO AUTOMÁTICO DE ATAQUES CONOCIDOS (fail2ban)
+  Rack::Attack.blocklist('fail2ban pentesters') do |req|
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
+      query = CGI.unescape(req.query_string)
+      path = req.path
 
+      query.include?('/etc/passwd') ||
+      path.include?('/etc/passwd') ||
+      path.include?('wp-admin') ||
+      path.include?('wp-login') ||
+      path.include?('StdOut.ReadAll') ||
+      path.include?('trace.ax') ||
+      path.include?('WAITFOR')
+    end
+  end
 
-# 	Rack::Attack.blocklist('fail2ban pentesters') do |req|
-# 	  # `filter` returns truthy value if request fails, or if it's from a previously banned IP
-# 	  # so the request is blocked
-# 	  Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", :maxretry => 3, :findtime => 10.minutes, :bantime => 5.minutes) do
-# 	    # The count for the IP is incremented if the return value is truthy
-# 	    CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
-# 	    req.path.include?('/etc/passwd') ||
-# 	    req.path.include?('wp-admin') ||
-# 	    req.path.include?('wp-login') ||
-# 	    req.path.include?('StdOut.ReadAll') ||
-# 	    req.path.include?('trace.ax') ||
-# 	    req.path.include?('WAITFOR')
+  # RESPUESTA PARA IPS BLOQUEADAS
+  Rack::Attack.blocklisted_response = lambda do |_env|
+    [503, {}, ['Blocked']]
+  end
 
-# 	  end
-# 	end
-# 	Rack::Attack.blocklisted_response = lambda do |env|
-# 	  # Using 503 because it may make attacker think that they have successfully
-# 	  # DOSed the site. Rack::Attack returns 403 for blocklists by default
-# 	  [ 503, {}, ['Blocked']]
-# 	end
-
-
-
-# 	# Rack::Attack.throttled_response = lambda do |env|
-# 	#   # NB: you have access to the name and other data about the matched throttle
-# 	#   #  env['rack.attack.matched'],
-# 	#   #  env['rack.attack.match_type'],
-# 	#   #  env['rack.attack.match_data']
-
-# 	#   # Using 503 because it may make attacker think that they have successfully
-# 	#   # DOSed the site. Rack::Attack returns 429 for throttling by default
-# 	#   [ 503, {}, ["Server Error\n"]]
-# 	# end
-
-# end
+  # RESPUESTA PERSONALIZADA PARA RATE LIMITING
+  Rack::Attack.throttled_response = lambda do |_env|
+    [503, {}, ["Server Busy\n"]]
+  end
+end
