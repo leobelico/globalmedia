95932b8dc31ab3851a718ba5bd5278197c92e4af
Sistemas <ldj821@gmail.com>
2025-08-25 09:34:07 -0600

    mejora en caracteres

--- app/controllers/panel/relationships_controller.rb
+++ app/controllers/panel/relationships_controller.rb
@@ -41,10 +41,7 @@ class Panel::RelationshipsController < ApplicationController
 	end
 
 	def show
-		@article_relationships = @relationship.article_relationships
-                           .includes(:article)
-                           .order(created_at: "DESC")
-                           .paginate(page: params[:page], per_page: 20)
+
 	end
 
 	def edit
