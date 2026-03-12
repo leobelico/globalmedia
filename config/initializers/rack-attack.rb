83097e23565a72f1e41d7c8b62827d40f3ed83c8
Sistemas <ldj821@gmail.com>
2026-03-12 15:29:53 -0600

    rack actualozado

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,19 +1,5 @@
 class Rack::Attack
 
-  # empresa
-  safelist('allow company') do |req|
-    req.ip == '187.189.250.162'
-  end
-
-  # bloquear bots agresivos
-  blocklist('bad bots') do |req|
-    req.user_agent =~ /SemrushBot|PetalBot|TikTokSpider|YandexBot|Amazonbot/i
-  end
-
-  # bloqueo ips malas
-  blocklist('block known bad IPs') do |req|
-    ['52.233.27.230','85.241.15.110','90.94.138.253'].include?(req.ip)class Rack::Attack
-
 safelist('allow company') do |req|
 req.ip == '187.189.250.162'
 end
@@ -47,14 +33,5 @@ end
 throttle('req/ip', limit: 100, period: 1.minute) do |req|
 req.ip
 end
-
-end
-
-  end
-
-  # limitar requests
-  throttle('req/ip', limit: 120, period: 1.minute) do |req|
-    req.ip
-  end
 
 end
