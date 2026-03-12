0fbda104ba7c91055a61a4733b66136d206f766e
Sistemas <ldj821@gmail.com>
2026-03-12 15:27:48 -0600

    rack actualozado

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -12,7 +12,44 @@ class Rack::Attack
 
   # bloqueo ips malas
   blocklist('block known bad IPs') do |req|
-    ['52.233.27.230','85.241.15.110','90.94.138.253'].include?(req.ip)
+    ['52.233.27.230','85.241.15.110','90.94.138.253'].include?(req.ip)class Rack::Attack
+
+safelist('allow company') do |req|
+req.ip == '187.189.250.162'
+end
+
+safelist('allow good bots') do |req|
+req.user_agent =~ /Googlebot|bingbot|facebookexternalhit|meta-externalagent/i
+end
+
+blocklist('bad bots') do |req|
+req.user_agent =~ /SemrushBot|PetalBot|TikTokSpider|YandexBot|Amazonbot|GPTBot|ClaudeBot|OAI-SearchBot/i
+end
+
+blocklist('hashtag scraping') do |req|
+req.path.include?('/articles/hashtag') &&
+req.user_agent =~ /bot|spider|crawler/i
+end
+
+blocklist('crazy pagination') do |req|
+page = req.params["page"].to_i
+page > 100
+end
+
+blocklist('block known bad IPs') do |req|
+[
+'52.233.27.230',
+'85.241.15.110',
+'90.94.138.253'
+].include?(req.ip)
+end
+
+throttle('req/ip', limit: 100, period: 1.minute) do |req|
+req.ip
+end
+
+end
+
   end
 
   # limitar requests
